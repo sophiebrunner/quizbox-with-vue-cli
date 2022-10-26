1e93149c88408f3881885e9f12848b7324edeceb
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-10-26 22:42:46 +0200

    feat: collapse menu after link was clicked

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,26 @@
       <div class="nav">
         <ul class="nav__list">
           <li class="nav__item">
-            <router-link :to="{ name: 'quizbox' }" class="text--light-colored"
+            <router-link
+              :to="{ name: 'quizbox' }"
+              class="text--light-colored"
+              @click="oncollapseMenu"
               >Play Quizbox</router-link
             >
           </li>
           <li class="nav__item">
-            <router-link :to="{ name: 'questions' }" class="text--light-colored"
+            <router-link
+              :to="{ name: 'questions' }"
+              class="text--light-colored"
+              @click="oncollapseMenu"
               >Browse Questions</router-link
             >
           </li>
           <li class="nav__item" v-if="sessionActive">
-            <router-link :to="{ name: 'session' }" class="text--light-colored"
+            <router-link
+              :to="{ name: 'session' }"
+              class="text--light-colored"
+              @click="oncollapseMenu"
               >Resume Session</router-link
             >
           </li>
